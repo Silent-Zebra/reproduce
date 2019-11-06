40556477a497371c18b89833b638473ecbe0b0b8
Sherry Yuan <154757929sherry@gmail.com>
2019-11-05 21:34:17 -0500

    initial commit: task 4

---
+++ are-16-heads-really-better-than-1/task4_generate_trend.py
@@ -0,0 +1,23 @@
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+total_head = 144
+f = open("MNLIprunesummary.txt", 'r')
+base_acc = float(f.readlines()[1].split()[-1])
+f.seek(0)
+f = open("MNLIprunesummary.txt", 'r')
+prune_accuracy_pair = re.findall(": (.*?)\t(.*?)\n", f.read())
+f.close()
+x = [float(pair[0])/total_head*100 for pair in prune_accuracy_pair]
+y = [float(pair[1]) for pair in prune_accuracy_pair]
+plt.style.use('seaborn-whitegrid')
+fig = plt.figure()
+ax = plt.axes()
+plt.xlabel('Precentage Pruned', fontsize=13)
+plt.ylabel('Accuracy', fontsize=13)
+plt.plot(x, y, color='blue', linestyle='-')
+#plt.plot(x, base_acc-np.array(y), color='g', linestyle='--')
+plt.show()
+
+
+
