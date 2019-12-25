4d8710f801a5510075f94d16d8fe6db794d4c5a0
Stephen Zhao <szstephenzhao88@gmail.com>
2019-12-25 15:17:49 -0500

    debugging

--- are-16-heads-really-better-than-1/fairseq/prune.py
+++ are-16-heads-really-better-than-1/fairseq/prune.py
@@ -134,6 +134,11 @@ def main(args):
         shard_id=args.distributed_rank,
     )
     # Load the latest checkpoint if one is available
+    print("DEBUGGING")
+    print(load_checkpoint)
+    print(args)
+    print(trainer)
+    print(epoch_itr)
     if not load_checkpoint(args, trainer, epoch_itr):
         trainer.dummy_train_step([dummy_batch])
 
