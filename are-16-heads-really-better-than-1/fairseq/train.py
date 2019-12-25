223081c6f57a2371aff7607b7b99257625dd8659
Stephen Zhao <szstephenzhao88@gmail.com>
2019-12-25 15:40:34 -0500

    debugging

--- are-16-heads-really-better-than-1/fairseq/train.py
+++ are-16-heads-really-better-than-1/fairseq/train.py
@@ -384,11 +384,17 @@ def save_checkpoint(args, trainer, epoch_itr, val_loss):
 def load_checkpoint(args, trainer, epoch_itr):
     """Load a checkpoint and replay dataloader to match."""
     os.makedirs(args.save_dir, exist_ok=True)
+    print("SAVEDIR")
+    print(args.save_dir)
     pure_restore_file = os.path.abspath(args.restore_file)
+    print(args.restore_file)
+    print(pure_restore_file)
     if os.path.isfile(pure_restore_file):
         checkpoint_path = pure_restore_file
     else:
         checkpoint_path = os.path.join(args.save_dir, args.restore_file)
+    print(checkpoint_path)
+    print(os.path.isfile(checkpoint_path))
     if os.path.isfile(checkpoint_path):
         extra_state = trainer.load_checkpoint(checkpoint_path, args.reset_optimizer, args.reset_lr_scheduler,
                                               eval(args.optimizer_overrides))
