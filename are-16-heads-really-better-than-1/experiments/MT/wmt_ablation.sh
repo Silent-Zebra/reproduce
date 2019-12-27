d212679778ad9b6918f604846b5edef6bce5c31c
Stephen Zhao <szstephenzhao88@gmail.com>
2019-12-27 15:11:10 -0500

    cleanup

--- are-16-heads-really-better-than-1/experiments/MT/wmt_ablation.sh
+++ are-16-heads-really-better-than-1/experiments/MT/wmt_ablation.sh
@@ -5,18 +5,16 @@
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 DATA_BIN=./wmt14_en-fr/wmt14.en-fr.joined-dict.newstest2014
 MODEL=./wmt14_en-fr/wmt14.en-fr.joined-dict.transformer/model.pt
-# DATA_BIN=/projects/tir3/users/pmichel1/data-bin/wmt14.en-fr.joined-dict.newstest2013
-# MODEL=/projects/tir3/users/pmichel1/checkpoints/wmt14.en-fr.joined-dict.transformer/model.pt
 MOSES_SCRIPTS="fairseq/examples/translation/mosesdecoder/scripts"
 OUT_DIR=output
 # tokenized en
 SRC_FILE="wmt14_en_fr_data/newstest2014.tok.en"
 REF_FILE="wmt14_en_fr_data/newstest2014.fr"
-# OUT_PREFIX="newstest2014_en-fr"
-# EXTRA_OPTIONS=$3
+OUT_PREFIX="newstest2014_en-fr"
+EXTRA_OPTIONS=$3
 # Use the following instead for ablating all but one head in a layer
-OUT_PREFIX=newstest2014_en-fr.allbut
-EXTRA_OPTIONS="--transformer-mask-all-but-one-head"
+# OUT_PREFIX=newstest2014_en-fr.allbut
+# EXTRA_OPTIONS="--transformer-mask-all-but-one-head"
 
 mkdir -p $OUT_DIR
 # Compute base BLEU
